94d83a73fadd4f00be43a0b7e233c7f8372f8f81
opengl-8080 <opengl-8080@users.noreply.github.com>
2023-03-09 22:11:54 +0900

    コピー先に同じ内容のファイルがある場合

--- src/main/java/backup/domain/destination/DestinationDirectory.java
+++ src/main/java/backup/domain/destination/DestinationDirectory.java
@@ -3,6 +3,7 @@ package backup.domain.destination;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class DestinationDirectory {
     private final Path dir;
@@ -14,7 +15,7 @@ public class DestinationDirectory {
         final Path destinationFile = dir.resolve(relativePath);
         try {
             Files.createDirectories(destinationFile.getParent());
-            Files.copy(originFile, destinationFile);
+            Files.copy(originFile, destinationFile, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/test/java/backup/domain/origin/OriginDirectoryTest.java
+++ src/test/java/backup/domain/origin/OriginDirectoryTest.java
@@ -11,14 +11,30 @@ class OriginDirectoryTest {
     @RegisterExtension
     TestFiles testFiles = new TestFiles();
 
+    final OriginDirectory origin = new OriginDirectory(testFiles.originDir());
+    final DestinationDirectory destination = new DestinationDirectory(testFiles.destinationDir());
+
     @Test
     void コピー先に何もファイルが無い場合() {
         testFiles.newOriginFile("test1.txt", "one");
         testFiles.newOriginFile("foo/test2.txt", "two");
         testFiles.newOriginFile("fizz/buzz/test3.txt", "three");
 
-        final OriginDirectory origin = new OriginDirectory(testFiles.originDir());
-        final DestinationDirectory destination = new DestinationDirectory(testFiles.destinationDir());
+        origin.backupTo(destination);
+
+        assertThat(testFiles.destinationFile("test1.txt")).hasContent("one");
+        assertThat(testFiles.destinationFile("foo/test2.txt")).hasContent("two");
+        assertThat(testFiles.destinationFile("fizz/buzz/test3.txt")).hasContent("three");
+    }
+
+    @Test
+    void コピー先に同じ内容のファイルがある場合() {
+        testFiles.newOriginFile("test1.txt", "one");
+        testFiles.newOriginFile("foo/test2.txt", "two");
+        testFiles.newOriginFile("fizz/buzz/test3.txt", "three");
+        testFiles.newDestinationFile("test1.txt", "one");
+        testFiles.newDestinationFile("foo/test2.txt", "two");
+        testFiles.newDestinationFile("fizz/buzz/test3.txt", "three");
 
         origin.backupTo(destination);
 
